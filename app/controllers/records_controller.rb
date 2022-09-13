9979ce3cbd773e03fda5594b00c7f332f6ee97e8
yossy2424 <yossy4415007@gmail.com>
2022-09-13 11:39:33 +0900

    体重記録機能の作成

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -3,5 +3,20 @@ class RecordsController < ApplicationController
   end
 
   def new
+    @record = Record.new
   end
+
+  def create
+    @record = Record.new(record_params)
+    if @record.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def record_params
+    params.require(:record).permit(:weight, :body_fat_percentage, :record_date).merge(user_id: current_user.id)
+end
 end
