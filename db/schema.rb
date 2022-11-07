0bf2b1eb0e6aec6cb516a0fda9a024b0824be7a5
yossy2424 <yossy4415007@gmail.com>
2022-11-07 11:43:28 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -46,11 +46,6 @@ ActiveRecord::Schema.define(version: 2022_10_31_032249) do
     t.index ["user_id"], name: "index_records_on_user_id"
   end
 
-  create_table "toppages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
